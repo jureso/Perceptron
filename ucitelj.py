573fe455dc3c289039351f2dc598a2728242d840
Jure Sokolic <jure.sokolic@gmail.com>
2012-06-12 15:32:22 +0200

    Razred Ucitelj

---
+++ ucitelj.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 12 15:21:07 2012
+
+@author: jureso
+"""
+
+class Ucitelj:
+    toleranca = 0.0000001
+    maxiter = 100000
+    
+
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
