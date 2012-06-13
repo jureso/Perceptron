e21716a502622f1edef93d7823c10cbe6a7f6bb1
Jure Sokolic <jure.sokolic@gmail.com>
2012-06-13 15:44:52 +0200

    V main dodane vrstice iz datoteke in popravljena 18.vrstica

--- ucitelj.py
+++ ucitelj.py
@@ -4,7 +4,7 @@ Created on Tue Jun 12 15:21:07 2012
 
 @author: jureso
 """
-
+import mmap
 from numpy  import *
 from perceptron import *
 from time import time
@@ -16,7 +16,7 @@ class Ucitelj:
         if isinstance(vhodi, str):
             print 'Nismo še tam'            
         self.n_vzorcev, self.dim_vzorcev = vhodi.shape
-        self.n_vzorcev, self.n_razredov = vhodi.shape
+        self.n_vzorcev, self.n_razredov = izhodi.shape
     def nastavi_ucenje(self, n2):
         self.P = Perceptron(self.dim_vzorcev, n2, self.n_razredov)
             
@@ -49,18 +49,45 @@ class Ucitelj:
             
 
 def main():
-    vhodi = array([[0, 1],[1, 0], [0,0], [1,1]])
-    izhodi = array([[1,0],[1,0],[0,1],[0,1]])
+    #vhodi = array([[0, 1],[1, 0], [0,0], [1,1]])
+    #izhodi = array([[1,0],[1,0],[0,1],[0,1]])
+    f = open('/home/jureso/Documents/Perceptron/isolet5.data', 'r')
+    lines = 0
+    buf_size = 1024 * 1024
+    read_f = f.read # loop optimization
+    buf = read_f(buf_size)
+    while buf:
+        lines += buf.count('\n')
+        buf = read_f(buf_size)
+    
+    
+    a = zeros((lines,618))
+    vhodi = zeros((lines, 617))
+    izhodi = zeros((lines, 26))
+    f.close()
+    f = open('/home/jureso/Documents/Perceptron/isolet5.data', 'r')
+    i = 0
+    
+    while (i >= 0):
+        s1 = f.readline()
+        if len(s1) == 0:
+            break
+        a[i,:] = fromstring(s1[0:-2], sep=', ')
+        i +=1
+        
+    for i in range(0,lines):
+        vhodi[i,:] = a[i,0:-1]
+        izhodi[i,a[i,-1]-1] = 1
     U = Ucitelj(vhodi,izhodi)
-    U.nastavi_ucenje(2)
+    U.nastavi_ucenje(500)
     dw, iteracij, cas = U.ucenje(vhodi,izhodi)
     print dw
     print iteracij
     print cas
-    print U.P.razvrsti(vhodi[0,:])
-    print U.P.razvrsti(vhodi[1,:])
-    print U.P.razvrsti(vhodi[2,:])
-    print U.P.razvrsti(vhodi[3,:])
+#    print U.P.razvrsti(vhodi[0,:])
+#    print U.P.razvrsti(vhodi[1,:])
+#    print U.P.razvrsti(vhodi[2,:])
+#    print U.P.razvrsti(vhodi[3,:])
     
     print U.testiraj(vhodi,izhodi)
 
